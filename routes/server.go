3c801140be2a74e39426b8e8e73cbaa423fb5ebb
SakaiTaka23 <tt3.sakai@gmail.com>
2020-10-16 19:06:08 +0900

    ルーティングについて学習(途中)

---
+++ routes/server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+// Handlers
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func createUser(c echo.Context) error {
+	return c.String(http.StatusOK, "create")
+}
+
+func findUser(c echo.Context) error {
+	return c.String(http.StatusOK, "find")
+}
+
+func updateUser(c echo.Context) error {
+	return c.String(http.StatusOK, "update")
+}
+
+func deleteUser(c echo.Context) error {
+	return c.String(http.StatusOK, "delete")
+}
+
+func main() {
+	e := echo.New()
+	// Routes
+	e.GET("/hello", hello)
+	e.Any("/any", func(c echo.Context) error {
+		return c.String(http.StatusOK, "any")
+	})
+	// エラーが出る
+	e.Match([]string{"GET", "POST"}, "/match", findUser)
+
+	e.POST("/users", createUser)
+	e.GET("/users", findUser)
+	e.PUT("/users", updateUser)
+	e.DELETE("/users", deleteUser)
+
+	e.Logger.Fatal(e.Start(":8000"))
+}
