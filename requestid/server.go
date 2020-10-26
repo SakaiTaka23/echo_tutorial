370e0c88002c808e54f784f9c4da69de18c9cce0
SakaiTaka23 <tt3.sakai@gmail.com>
2020-10-26 14:04:43 +0900

    requestidの学習

---
+++ requestid/server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	e := echo.New()
+	// e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+	// 	Format: "method=${method}, uri=${uri}, status=${status}\n",
+	// }))
+	e.Use(middleware.RequestID())
+
+	// e.Use(middleware.BodyDump(func(c echo.Context, reqBody, resBody []byte) {
+	// 	fmt.Printf("Request Body: %v\n", string(reqBody))
+	// 	fmt.Printf("Response Body: %v\n", string(resBody))
+	// }))
+
+	e.GET("/", func(c echo.Context) error {
+		output := fmt.Sprintf("%#v", c.Request().Header)
+		fmt.Print(output)
+
+		return c.NoContent(http.StatusOK)
+	})
+
+	e.Logger.Fatal(e.Start(":8000"))
+}
